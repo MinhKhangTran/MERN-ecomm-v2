767b9e62f1a9081963f7a6d152100130e9a2c3ee
Minh Khang <minhkhang.tran@gmx.de>
2021-03-13 11:06:39 +0100

    deploy ready

--- server/server.js
+++ server/server.js
@@ -44,6 +44,19 @@ app.use("/api/a1/uploads", uploadRoutes);
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
+// for deploying
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
 // error middleware
 app.use(notFound);
 app.use(errorHandler);
